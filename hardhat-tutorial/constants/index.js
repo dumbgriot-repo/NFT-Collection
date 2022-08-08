40e7aaaf604e37c586e37f118bffbcab49538cea
dumbgriot-repo <dumbgriot@gmail.com>
2022-08-08 10:27:43 -0700

    initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xfB143c0A6575ec2bD6602F8703c4773b907588D5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
